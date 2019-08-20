a8941ec788cc8cb0e445171ae70c2513fd77b5d5
Adrian Carpenter <adriatic.c@gmail.com>
2019-08-19 17:54:25 -0700

    Build script can run as another user on its own

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# Change to the directory of the script itself and get a username based on
+# the path where the file resides. This assumes the typical Linux scheme of
+# /home/username and will otherwise fail. The script then re-runs itself as 
+# that user.
+
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+if [ "$(id -u)" = "0" ]; then
+    USER="$(echo $PWD|sed 's|^/home/\([^/]\+\).*$|\1|')"
+    SCRIPT="$(realpath $0)"
+    exec su -l -c "$SCRIPT" "$USER"
+fi
+
+
+# Build process goes here
+
 alias yarn=yarnpkg
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 MD5="$(md5sum package.json)"
 git pull || exit
