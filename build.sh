78eb092050074a0f81ff147dc4f7c0dd334b20fa
Adrian Carpenter <adriatic.c@gmail.com>
2019-08-19 17:12:41 -0700

    Fix build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 alias yarn=yarnpkg
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-MD5=$(md5sum package.json)
+MD5="$(md5sum package.json)"
 git pull || exit
-[ $MD5 = $(md5sum package.json) ] || yarn install
+[ "$MD5" = "$(md5sum package.json)" ] || yarn install
 yarn build
