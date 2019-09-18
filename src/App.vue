432e12032b7af177987f6a30f829ebe736bbd119
adrian-the-git <adriatic.c@gmail.com>
2019-09-18 13:14:45 -0700

    Update version to v1.0.6

--- src/App.vue
+++ src/App.vue
@@ -84,7 +84,7 @@
           </a>
           <a :href="content.mailto" target="_blank" class="db spaced">Contact</a>
           <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="CC BY-NC-SA 4.0" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
-          <p class="dib">Beautiful Trouble 2019, solidaritynotaid.com v1.0.5</p>
+          <p class="dib">Beautiful Trouble 2019, solidaritynotaid.com v1.0.6</p>
         </div>
       </div>
     </footer>
